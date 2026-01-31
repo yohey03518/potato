86fdf71a103227693248960e1aa31e89ed7a1293
yohey <yohey03518@gmail.com>
2026-01-31 16:36:15 +0800

    refactor: refactor and add rider related files

--- src/Potato.Client/Services/StockPriceMonitorService.cs
+++ src/Potato.Client/Services/StockPriceMonitorService.cs
@@ -4,45 +4,35 @@ using Potato.Client.External.Fugle;
 
 namespace Potato.Client.Services;
 
-public class StockPriceMonitorService : BackgroundService
+public class StockPriceMonitorService(
+    IFugleApiClient fugleApiClient,
+    ILogger<StockPriceMonitorService> logger,
+    IHostApplicationLifetime hostApplicationLifetime)
+    : BackgroundService
 {
-    private readonly IFugleApiClient _fugleApiClient;
-    private readonly ILogger<StockPriceMonitorService> _logger;
-    private readonly IHostApplicationLifetime _hostApplicationLifetime;
-
-    public StockPriceMonitorService(
-        IFugleApiClient fugleApiClient, 
-        ILogger<StockPriceMonitorService> logger,
-        IHostApplicationLifetime hostApplicationLifetime)
-    {
-        _fugleApiClient = fugleApiClient;
-        _logger = logger;
-        _hostApplicationLifetime = hostApplicationLifetime;
-    }
-
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        _logger.LogInformation("StockPriceMonitorService is starting.");
+        logger.LogInformation("StockPriceMonitorService is starting.");
 
         try
         {
             if (!stoppingToken.IsCancellationRequested)
             {
                 var symbolId = "2330";
-                _logger.LogInformation("Fetching stock price for {SymbolId}...", symbolId);
+                logger.LogInformation("Fetching stock price for {SymbolId}...", symbolId);
                 
-                var result = await _fugleApiClient.GetIntradayQuoteAsync(symbolId);
+                var result = await fugleApiClient.GetIntradayQuoteAsync(symbolId);
                 
-                _logger.LogInformation("Successfully fetched stock price for {SymbolId}.", symbolId);
-                _logger.LogInformation("Result: {Result}", result);
+                logger.LogInformation("Successfully fetched stock price for {SymbolId}.", symbolId);
+                logger.LogInformation("Result: {Result}", result);
             }
         }
         catch (Exception ex)
         {
-            _logger.LogError(ex, "An error occurred while fetching the stock price.");
+            logger.LogError(ex, "An error occurred while fetching the stock price.");
         }
 
-        _logger.LogInformation("StockPriceMonitorService has completed its task. Shutting down application...");
-        _hostApplicationLifetime.StopApplication();
+        logger.LogInformation("StockPriceMonitorService has completed its task. Shutting down application...");
+        hostApplicationLifetime.StopApplication();
     }
 }
